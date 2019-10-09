3cbe99549a55e602ad19e5f071936d681d730ea7
chandu1504 <45807155+chandu1504@users.noreply.github.com>
2019-10-10 01:41:49 +0530

    Add files via upload

--- port.py
+++ port.py
@@ -5,13 +5,13 @@ import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 host = '127.0.0.1'
-port = 200
+port = 111
 
 def portscanning(port):
         if sock.connect_ex((host,port)):
-                print ("port %d is open"  % (port))
+                print ("port %d is closed"  % (port))
         else:
-                print ("port %d is closed" % (port))
+                print ("port %d is open" % (port))
 
 portscanning(port)
 
